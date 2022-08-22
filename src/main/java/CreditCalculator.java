e27279e7200b752f1e613c15c56c57f3c7dfaba1
Tural Guluev <tural-98@bk.ru>
2022-08-22 13:10:01 +0300

    the program works correctly. Compiles a csv file of the payment schedule

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -3,9 +3,13 @@ import com.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DecimalFormat;
 import java.time.Month;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class CreditCalculator {
     private final int numberOfMonthsInAYear = 12;
@@ -14,62 +18,97 @@ public class CreditCalculator {
     private double loanAmount;
     private int duration;
     private double monthlyInterestAmount;
-    private DecimalFormat decimalFormat;
     private GregorianCalendar calendar;
     private double balanceOwed;
+    private double monthlyPayment;
+    private File paymentSchedule;
 
     public CreditCalculator(double loanAmount, double interestRate, int duration) {
         this.loanAmount = loanAmount;
         this.interestRate = interestRate;
         this.duration = duration;
-        decimalFormat = new DecimalFormat("#.####");
-        monthlyInterestAmount = Double.parseDouble(decimalFormat.format((interestRate / numberOfMonthsInAYear) / 100).replaceAll(",", "."));
+
+        monthlyInterestAmount = Double.parseDouble(changingTheFractionalPart(4).format((interestRate / numberOfMonthsInAYear) / 100).replaceAll(",", "."));
         calendar = new GregorianCalendar();
-        balanceOwed = loanAmount;
+        balanceOwed = Double.parseDouble(changingTheFractionalPart(2).format(loanAmount));
+
+        this.monthlyPayment = Double.parseDouble(
+                changingTheFractionalPart(2).format(
+
+                        loanAmount *
+                                (monthlyInterestAmount *
+                                        (Math.pow(1 + monthlyInterestAmount, duration))) /
+                                (Math.pow(1 + monthlyInterestAmount, duration) - 1)).replaceAll(",", "."));
+
+        paymentSchedule = new File("result.csv");
+        if (paymentSchedule.length() != 0) {
+            paymentSchedule.delete();
+
+        }
+
     }
 
 
     public void getLoanInformation() {
-        if (loanAmount == 0) {
+//        if (balanceOwed < monthlyPayment) {
+//            return;
+//        }
+
+
+        double accruedInterest = Double.parseDouble(changingTheFractionalPart(2).
+                format(balanceOwed * monthlyInterestAmount)
+                .replaceAll(",", "."));
+
+        double principalDebt = Double.parseDouble(changingTheFractionalPart(2).
+                format(monthlyPayment - accruedInterest).
+                replaceAll(",", "."));
+
+        if (balanceOwed < monthlyPayment) {
+            balanceOwed = 0;
+            saveTheResultToAFile(monthlyPayment, accruedInterest, principalDebt, balanceOwed, monthCount++);
+
             return;
         }
+        balanceOwed = Double.parseDouble(changingTheFractionalPart(2).
+                format(balanceOwed - principalDebt).replaceAll(",", "."));
 
-        double monthlyPayment = loanAmount * Double.parseDouble(new DecimalFormat("#.######").format(
-                (monthlyInterestAmount *
-                        (Math.pow(1 + monthlyInterestAmount, duration))) /
-                        (Math.pow(1 + monthlyInterestAmount, duration) - 1)).replaceAll(",", "."));
-
-        double principalDebt = balanceOwed * monthlyInterestAmount;
-
-        balanceOwed = loanAmount - monthlyPayment;
-
-        saveTheResultToAFile(monthlyPayment, principalDebt,balanceOwed, monthCount++);
-
+        saveTheResultToAFile(monthlyPayment, accruedInterest, principalDebt, balanceOwed, monthCount++);
+        calendar.add(Calendar.MONTH, +1);
 
         getLoanInformation();
 
     }
 
 
-    public void saveTheResultToAFile(double monthlyPayment, double principalDebt, double balanceOwed, int monthCount) {
-        //общая сумма кредита,ежемесячный платеж, ежемесячные проценты, ,кол-во месяцеоы
+    public void saveTheResultToAFile(double monthlyPayment, double accruedInterest, double principalDebt, double balanceOwed, int monthCount) {
 
         File resultFile = new File("result.csv");
 
-        try (CSVWriter writer = new CSVWriter(new FileWriter(resultFile))) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(resultFile, true))) {
             if (resultFile.length() == 0) {
                 writer.writeNext(new String[]{"№ платежа", "Дата платежа", " Сумма платежа", " Основной долг", " Начисленные проценты", " Остаток задолженности"});
             }
             writer.writeNext(new String[]{
-                    String.valueOf(monthCount),//n
-                    Month.of(monthCount).name(),//date
+                    String.valueOf(monthCount),
+                    calendar.getDisplayName(Calendar.MONTH,
+                            Calendar.LONG, new Locale("ru")),
                     String.valueOf(monthlyPayment),
-                    String.valueOf(monthlyPayment - principalDebt),//основной долг
-                    String.valueOf(principalDebt),//начисленные проценты
+                    String.valueOf(principalDebt),
+                    String.valueOf(accruedInterest),
                     String.valueOf(balanceOwed)});
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public DecimalFormat changingTheFractionalPart(int numberOfDecimalPlaces) {
+        StringBuilder pattern = new StringBuilder("#.");
+        for (int a = 0; a < numberOfDecimalPlaces; a++) {
+            pattern.append("#");
+        }
+
+        return new DecimalFormat(pattern.
+                toString());
     }
 }
